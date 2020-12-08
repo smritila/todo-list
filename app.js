7af4731820c6450cba458ee016eb78985ca8da1e
Smritikana Ghoshal <smrtghoshal24@gmail.com>
2020-12-08 20:06:30 +0530

    implements delete button functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Selectors
 document.querySelector('form').addEventListener('submit', handleSubmitForm);
-document.querySelector('ul').addEventListener('click', handleClickDeleteOrCheck)
+document.querySelector('ul').addEventListener('click', handleClickDeleteOrCheck);
+document.getElementById('clearAll').addEventListener('click',handleClearAll);
 // Event Handlers
 function handleSubmitForm(e){
   e.preventDefault();
@@ -14,8 +15,15 @@ function handleClickDeleteOrCheck(e) {
      if(e.target.name=='checkButton')
         checkTodo(e);
 
-    if(e.target.name='deleteButton')    
+    if(e.target.name=='deleteButton')    
        deleteTodo(e);
+}
+
+function handleClearAll(e)
+{
+    document.querySelector('ul').innerHTML='';
+
+
 }
 
 // Helpers
@@ -44,7 +52,13 @@ function checkTodo(e) {
 }
 
 function deleteTodo(e) {
+     let item = e.target.parentNode;
 
+     item.addEventListener('transitioned', function(){
+         item.remove();
+     })
+
+     item.classList.add('todo-list-item-fall');
 
 }
 
